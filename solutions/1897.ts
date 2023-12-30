b37dd45b4b002ace65a70a9cce43dc239bc2673f
Xavier Fujii <nothingtoworry7@gmail.com>
2023-12-31 02:06:04 +0800

    1897 TypeScript

---
+++ solutions/1897.ts
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/description/
+
+function makeEqual(words: string[]): boolean {
+    const wordsChars = words.map((w) => w.split(""));
+    const charsFrequenciesMap = new Map<string, number>();
+    for (const w of wordsChars) {
+        for (const c of w) {
+            const v = charsFrequenciesMap.get(c) ?? 0;
+            charsFrequenciesMap.set(c, v + 1);
+        }
+    }
+    for (const f of charsFrequenciesMap.values()) {
+        if (f % words.length !== 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(makeEqual(["abca", "bc"]));
+console.log(makeEqual(["abc", "aabc", "c"]));
