4b5ef7b2dde79e54bf54047820d16d25402fe746
Xavier Fujii <nothingtoworry7@gmail.com>
2023-12-31 18:17:01 +0800

    0055 of top150

---
+++ leetcode/0055.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/jump-game
+function canJump(nums: number[]): boolean {
+    let curFurthestIdx = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (i > curFurthestIdx) break;
+        curFurthestIdx = Math.max(curFurthestIdx, nums[i] + i);
+    }
+
+    const result = curFurthestIdx >= nums.length - 1;
+    return result;
+}
+
+canJump([2, 3, 1, 1, 4]);
