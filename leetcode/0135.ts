95a5422f71cbf73c3ee5a1aff4e95ab0067f6759
Xavier Fujii <nothingtoworry7@gmail.com>
2024-01-03 22:54:51 +0800

    0135 of top150

---
+++ leetcode/0135.ts
@@ -0,0 +1,16 @@
+function candy(ratings: number[]): number {
+    const results = new Array(ratings.length).fill(1)
+    for (let i = 1; i < ratings.length; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            results[i] = results[i - 1] + 1
+        }
+    }
+
+    for (let i = ratings.length - 1; i > 0; i--) {
+        if (ratings[i - 1] > ratings[i]) {
+            results[i - 1] = Math.max(results[i] + 1, results[i - 1])
+        }
+    }
+
+    return results.reduce((acc, cur) => acc + cur, 0)
+}
